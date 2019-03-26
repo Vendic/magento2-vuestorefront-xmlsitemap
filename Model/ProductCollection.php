56eb6907216b8d279a7157baa681fd68c62426c8
Tjitse Efdé <tjitse@vendic.nl>
2019-03-26 11:07:54 +0100

    Fix issue with Magento 2.2 product collections

--- Model/ProductCollection.php
+++ Model/ProductCollection.php
@@ -11,6 +11,7 @@ namespace Vendic\VueStorefrontSitemap\Model;
 use Magento\Catalog\Model\Product\Attribute\Source\Status;
 use Magento\Catalog\Model\Product\Visibility;
 use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory;
+use Magento\Store\Model\StoreManagerInterface;
 
 class ProductCollection
 {
@@ -27,8 +28,13 @@ class ProductCollection
      * @var Visibility
      */
     protected $productVisibility;
+    /**
+     * @var StoreManagerInterface
+     */
+    protected $storeManager;
 
     public function __construct(
+        StoreManagerInterface $storeManager,
         Status $productStatus,
         CollectionFactory $productCollectionFactory,
         Visibility $productVisibility
@@ -36,17 +42,31 @@ class ProductCollection
         $this->productCollectionFactory = $productCollectionFactory;
         $this->productStatus = $productStatus;
         $this->productVisibility = $productVisibility;
+        $this->storeManager = $storeManager;
     }
 
+    /**
+     * Get active products
+     * @return \Magento\Catalog\Model\ResourceModel\Product\Collection
+     */
     public function get()
     {
         $collection = $this->productCollectionFactory->create();
         // We need to add store filters first to avoid an Zend_Db_Statement_Exception https://github.com/magento/magento2/issues/15187
-        $collection->addStoreFilter(1); // TODO make configurable
+        $collection->addStoreFilter($this->getDefaultStoreId());
         $collection->addAttributeToSelect('url_key');
         $collection->addAttributeToFilter('status', ['in' => $this->productStatus->getVisibleStatusIds()]);
         $collection->setVisibility($this->productVisibility->getVisibleInSiteIds());
 
         return $collection;
+    }
+
+    /**
+     * TODO make store ID configurable via Magento backend
+     * @return int
+     */
+    private function getDefaultStoreId()
+    {
+        return $this->storeManager->getDefaultStoreView()->getId();
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # VueStorefront XML sitemap generator for Magento 2
+This modules generates a sitemap xml for VueStorefront projects that are integrated with Magento 2. VueStorefront uses a special url structure, based on Magento 2 data:
+
+**Categories:**
+
+https://vuestorefronturl.com/urlkey-id
+
+**Products:**
+
+https://vuestorefronturl.com/sku/urlkey
+
+## Installation
+```
+composer require composer require vendic/magento2-vuestorefront-xmlsitemap
+```
+
+Go to system configuration:
+> Stores > Configuration > Vendic > VueStorefront
+
+And set your Vuestorefront homepage url.
+
+### Contributors
+[Tjitse Efde](https://vendic.nl)
+
+### License
+The MIT License (MIT). Please see [License File](LICENSE.md) for more information.
