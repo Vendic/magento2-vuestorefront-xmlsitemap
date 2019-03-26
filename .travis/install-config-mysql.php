60ac6a4767e3b6428d6d58b1d17cee4369771a04
Tjitse Efdé <tjitse@vendic.nl>
2019-03-26 13:01:04 +0100

    Trigger travis build

---
+++ .travis/install-config-mysql.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'db-host' => '127.0.0.1',
+    'db-user' => 'root',
+    'db-password' => '',
+    'db-name' => 'magento_2_travis',
+    'db-prefix' => '',
+    'backend-frontname' => 'backend',
+    'admin-user' => \Magento\TestFramework\Bootstrap::ADMIN_NAME,
+    'admin-password' => \Magento\TestFramework\Bootstrap::ADMIN_PASSWORD,
+    'admin-email' => \Magento\TestFramework\Bootstrap::ADMIN_EMAIL,
+    'admin-firstname' => \Magento\TestFramework\Bootstrap::ADMIN_FIRSTNAME,
+    'admin-lastname' => \Magento\TestFramework\Bootstrap::ADMIN_LASTNAME,
+];
